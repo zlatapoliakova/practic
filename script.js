bb9cb6daf9372d621cb45c1163487720dd3095f7
Zlata <zlata@gmail.com>
2024-08-26 17:33:38 +0300

    add script for movies

---
+++ script.js
@@ -0,0 +1,20 @@
+const numberOfFilms = +prompt("Скільки фільмів ви передивились?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const firstLastMovie = prompt("Один з останніх фільмів які ви передивились?", "");
+const firstLastMovieRating = +prompt("На скільки оцінити його?", "")
+
+const secondLastMovie = prompt("Один з останніх фільмів які ви передивились?", "");
+const secondLastMovieRating = +prompt("На скільки оцінити його?", "")
+
+personalMovieDB.movies[firstLastMovie] = firstLastMovieRating;
+personalMovieDB.movies[secondLastMovie] = secondLastMovieRating;
+
+console.log(personalMovieDB);
