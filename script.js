dd8ac2c020d12005d8b00674c953abd06d075572
Zlata <zlata@gmail.com>
2024-08-28 17:23:36 +0300

    add validation

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
-const numberOfFilms = +prompt("Скільки фільмів ви передивились?", "");
+let numberOfFilms; 
+while (!numberOfFilms || numberOfFilms === 1){
+    numberOfFilms = +prompt("Скільки фільмів ви передивились?", "");
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,13 +13,26 @@ const personalMovieDB = {
     privat: false
 };
 
-const firstLastMovie = prompt("Один з останніх фільмів які ви передивились?", "");
-const firstLastMovieRating = +prompt("На скільки оцінити його?", "")
+for (let i = 0; i < 2; i++) {
+    const lastMovie = prompt("Один з останніх фільмів які ви передивились?", "");
+     const lastMovieRating = +prompt("На скільки оцінити його?", "");
+    if (lastMovie != null && lastMovieRating != null && lastMovie != '' && lastMovieRating != '' && lastMovie.length < 50) {
+        personalMovieDB.movies[lastMovie] = lastMovieRating;
+        console.log('done')
+    } else {
+        console.log('error')
+        i--;
+    }
+}
 
-const secondLastMovie = prompt("Один з останніх фільмів які ви передивились?", "");
-const secondLastMovieRating = +prompt("На скільки оцінити його?", "")
-
-personalMovieDB.movies[firstLastMovie] = firstLastMovieRating;
-personalMovieDB.movies[secondLastMovie] = secondLastMovieRating;
-
+if (personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов")
+} else if ( 10 < personalMovieDB.count < 30) {
+    alert("Ви класический зритель")
+} else if ( personalMovieDB.count > 30) {
+    alert("Ви киноман")
+} else {
+    alert("Произошла ошибка")
+}
 console.log(personalMovieDB);
+
